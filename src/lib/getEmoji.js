b739579e605e7f1b28b3e956a7a76dd80cd0d9cb
Juan Manuel Romera Ferrio <juanmanuelromeraferrio@juans-mbp.lan>
2023-03-10 23:10:23 +0100

    find just lower case words

--- src/lib/getEmoji.js
+++ src/lib/getEmoji.js
@@ -3,7 +3,7 @@ const prisma = require('./prisma');
 const getEmoji = async (word) => {
     const entry = await prisma.emoji.findUnique({
         where: {
-            word: word
+            word: word.toLowerCase()
         }
     });
     return entry ? entry.emoji : null;
