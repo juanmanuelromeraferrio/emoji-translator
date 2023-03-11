05f220dfafc23ad190dd528a55e03dbdccd7fe65
Juan Manuel Romera Ferrio <juanmanuelromeraferrio@juans-mbp.lan>
2023-03-11 17:10:25 +0100

    save in lowecase and trim

--- src/lib/saveEmoji.js
+++ src/lib/saveEmoji.js
@@ -3,7 +3,7 @@ const prisma = require('./prisma');
 const saveEmoji = async (word, emoji, source) => {
     await prisma.emoji.create({
         data: {
-            word,
+            word: word.toLowerCase().trim(),
             emoji,
             source
         },
