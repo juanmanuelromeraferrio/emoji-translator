98ca43b65b32c3a461a905ac418433b5f353a02b
Juan Manuel Romera <juanmanuel.romeraferrio@gmail.com>
2023-03-18 17:59:11 +0100

    update github link

--- __tests__/Footer.test.js
+++ __tests__/Footer.test.js
@@ -14,8 +14,8 @@ describe('Footer', () => {
         expect(twitterLink).toBeInTheDocument();
         expect(twitterLink).toHaveAttribute('href', 'https://twitter.com/juanmaromfe');
 
-        const githubLink = screen.getByRole('link', { name: /juan on github/i });
+        const githubLink = screen.getByRole('link', { name: /emoji translator on github/i });
         expect(githubLink).toBeInTheDocument();
-        expect(githubLink).toHaveAttribute('href', 'https://github.com/juanmanuelromeraferrio');
+        expect(githubLink).toHaveAttribute('href', 'https://github.com/juanmanuelromeraferrio/emoji-translator');
     });
 });
