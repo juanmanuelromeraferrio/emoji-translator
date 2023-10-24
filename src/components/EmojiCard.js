121266a50c3115567e348e679b2b32c580c54ace
Juan Manuel Romera Ferrio <jmromera@copyright.com>
2023-10-24 18:25:34 +0200

    feat: improve style

--- src/components/EmojiCard.js
+++ src/components/EmojiCard.js
@@ -1,11 +1,29 @@
+import React, { useState, useEffect } from "react";
 import styles from "../styles/EmojiCard.module.css";
 
 const EmojiCard = ({ emoji, word }) => {
+
+    const [copied, setCopied] = useState(false);
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(emoji);
+        setCopied(true);
+    };
+
+    useEffect(() => {
+        if (copied) {
+            const timer = setTimeout(() => {
+                setCopied(false);
+            }, 1000);
+
+            return () => clearTimeout(timer);
+        }
+    }, [emoji, copied]);
+
     return (
-        <div className={styles.emojiCard}>
+        <button className={`${styles.emojiCard} ${copied ? styles.copy : ''}`} onClick={copyToClipboard}>
             <p className={styles.emoji}>{emoji}</p>
-            <p className={styles.word}>{word}</p>
-        </div>
+            <p className={styles.word}>{copied ? 'copied' : word}</p>
+        </button>
     );
 }
 
